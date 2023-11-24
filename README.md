64f8e31c8943837d2588952495c753d00a05f79b
Konstantin Dyachenko <drakonard@gmail.com>
2023-11-24 21:42:26 +0300

    Loading of different documents is supported

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # russian-law
+
+## Load latest constitution.html
+```sh
+node load-html.js --name=constitution --source-document-id=102027595
+```
+
+## Load latest criminal-code.html
+```sh
+node load-html.js --name="criminal-code" --source-document-id=102041891
+```
--- load-html.js
+++ load-html.js
@@ -5,18 +5,34 @@ const { JSDOM } = jsdom;
 const fs = require('fs');
 const path = require('path')
 const { saveFile } = require('./files.js');
-const url = "http://pravo.gov.ru/proxy/ips/?doc_itself=&nd=102027595&page=1&rdk=20&link_id=0#I0";
+const { program } = require('commander');
+
+program
+    .option('-d, --directory <type>', 'Directory', './data/html')
+    .option('-n, --name <type>', 'File name (required)')
+    .option('-e, --extension <type>', 'File extension', '.html')
+    .option('-s, --source-document-id <type>', 'Source Document ID (required)')
+    .parse(process.argv);
+
+const options = program.opts();
+if (!options.name || !options.sourceDocumentId) {
+  console.log('--name and --source-document-id are required');
+  process.exit(1);
+}
+const fileName = options.name + options.extension
+const directory = options.directory.endsWith('/') ? options.directory : options.directory + '/';  
 let html = '';
+
+const url = `http://pravo.gov.ru/proxy/ips/?doc_itself=&nd=${options.sourceDocumentId}&fulltext=1`;
 
 axios({
     url: url,
     method: 'GET',
-    responseType: 'arraybuffer'  // указываем, что ответ должен быть в виде ArrayBuffer
+    responseType: 'arraybuffer'
 })
   .then(function (response) {
-    html = iconv.decode(Buffer.from(response.data), 'win1251'); // декодируем данные с win-1251 в utf-8
-    saveFile('./data/html/constitution.html', html);
-    // console.log(html);
+    html = iconv.decode(Buffer.from(response.data), 'win1251');
+    saveFile(directory + fileName , html);
   })
   .catch(function (error) {
     console.error("Ошибка при загрузке страницы:", error);
