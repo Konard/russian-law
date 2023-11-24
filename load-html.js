37833e766f3c121454f9eefab7e05cccc5e2aae0
Konstantin Dyachenko <drakonard@gmail.com>
2023-11-25 01:25:39 +0300

    Better HTML transform, index.html added as a preparation for GitHub Pages

--- load-html.js
+++ load-html.js
@@ -6,6 +6,8 @@ const fs = require('fs');
 const path = require('path')
 const { saveFile } = require('./files.js');
 const { program } = require('commander');
+const cheerio = require('cheerio');
+const beautify = require('js-beautify').html;
 
 program
     .option('-d, --directory <type>', 'Directory', './data/html')
@@ -25,6 +27,25 @@ let html = '';
 
 const url = `http://pravo.gov.ru/proxy/ips/?doc_itself=&nd=${options.sourceDocumentId}&fulltext=1`;
 
+function transformHtml(html) {
+  html = html.replace(/^.*<div\s*id="text_content"\s*>\s*/s, '');
+  html = html.replace(/\s*<\/div>\s*<\/div>\s*<\/body>\s*<\/html>\s*$/s, '');
+  html = html.replaceAll('windows-1251', 'utf-8');
+
+  html = html.replaceAll('?docbody', 'http://pravo.gov.ru/proxy/ips/?docbody'); // обратные ссылки на источник
+
+  const $ = cheerio.load(html);
+
+  $('html').attr('style', 'display: table; margin: auto;');
+  $('body').attr('style', `font-size: 18px; line-height: 125%; word-wrap: break-word; display: table-cell; vertical-align: middle;`);
+
+  html = beautify($.html(), { indent_size: 2 });
+
+  html = html.replaceAll('font-family: "times new roman", times, serif;', 'font-family: Helvetica, sans-serif;');
+  
+  return html;
+}
+
 axios({
     url: url,
     method: 'GET',
@@ -32,11 +53,7 @@ axios({
 })
   .then(function (response) {
     html = iconv.decode(Buffer.from(response.data), 'win1251');
-
-    html = html.replace(/^.*<div\s*id="text_content"\s*>\s*/s, '');
-    html = html.replace(/\s*<\/div>\s*<\/div>\s*<\/body>\s*<\/html>\s*$/s, '');
-    html = html.replace('windows-1251', 'utf-8');
-
+    html = transformHtml(html);
     saveFile(directory + fileName , html);
   })
   .catch(function (error) {
