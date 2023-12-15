416495137d648476c3abe926ffe8533a3c0ad5b9
Konstantin Dyachenko <drakonard@gmail.com>
2023-12-15 05:36:19 +0300

    Add json-to-articles converter Add articles array for criminal law

---
+++ json-to-articles.js
@@ -0,0 +1,31 @@
+const jsdom = require("jsdom");
+const { JSDOM } = jsdom;
+const fs = require('fs');
+const path = require('path')
+const { saveFile } = require('./files.js');
+
+function jsonToArticles(law) {
+    const result = [];
+    // console.log(law.sections.length);
+    for (const section of law.sections) {
+      for (const chapter of section.chapters) {
+        for (const article of chapter.articles) {
+          const articleText = `${article.title}\n${article.clauses.join("\n")}`;
+          console.log(articleText);
+          result.push(articleText);
+        }
+      }
+    }
+    return result;
+}
+
+// const documentId = 102027595;
+const documentId = 102041891;
+
+const law = JSON.parse(fs.readFileSync(`./data/json/${documentId}.json`, 'utf8'));
+
+const json = jsonToArticles(law);
+// console.log('json', json);
+// console.log('json.sections[0].chapters', json.sections[0].chapters);
+
+saveFile(`./data/json/${documentId}.articles.json`, JSON.stringify(json, null, 2));
