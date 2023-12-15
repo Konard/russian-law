ad0310b8e1f98d5c31d001fef8cfb3f1fb0ad652
Konstantin Dyachenko <drakonard@gmail.com>
2023-12-15 05:23:31 +0300

    Update html-to-json algorithm: Update json version of constitution Update json version of criminal codex

--- html-to-json.js
+++ html-to-json.js
@@ -6,50 +6,76 @@ const { saveFile } = require('./files.js');
 
 // Преобразование HTML к JSON
 function htmlToJson(html) {
-  const result = { preamble: [], sections: [] };
-  const dom = new JSDOM(html);
-  const paragraphs = [...dom.window.document.querySelectorAll("p")];
+    const result = { preamble: [], sections: [] };
+    const dom = new JSDOM(html);
+    const paragraphs = [...dom.window.document.querySelectorAll("p")];
 
-  let preambleMode = true;
-  let currentSection;
-  let currentChapter;
-  let currentArticle;
+    let currentSection = null;
+    let currentChapter = null;
+    let currentArticle = null;
+    let preambleMode = true;
 
-  for(const p of paragraphs) {
-      // Если параграф пустой, пропускаем
-      if(!p.textContent.trim()) continue;
+    for (const p of paragraphs) {
+        const text = p.textContent.trim();
+        if (!text) continue;
 
-      if(p.classList.contains("T") || p.classList.contains("C")) {
-          preambleMode = false; // Конец преамбулы
-          
-          const text = p.textContent.trim();
+        if (preambleMode && !p.classList.contains("H") && !p.classList.contains("T")) {
+            result.preamble.push(text);
+            continue;
+        }
 
-          if(text.startsWith("РАЗДЕЛ")) {
-              currentSection = { title: text, chapters: [] }; // Новый раздел
-              currentChapter = null; // Нет текущей главы пока не встретим новую
-              result.sections.push(currentSection);
-          } else if(text.startsWith("ГЛАВА") && currentSection) {
-              currentChapter = { title: text, articles: [] }; // Новая глава
-              currentArticle = null;
-              currentSection.chapters.push(currentChapter);
-          } else if(text.startsWith("Статья") && currentChapter) {
-              currentArticle = { title: text, clauses: [] }; // Новая статья
-              currentChapter.articles.push(currentArticle);
-          }
-      } else if(preambleMode) {
-          result.preamble.push(p.textContent.trim());
-      } else if(currentArticle) {
-          currentArticle.clauses.push(p.textContent.trim());
-      }
-  }
+        if (p.classList.contains("H") || p.classList.contains("T")) {
+            preambleMode = false;
 
-  return result;
+            if (text.startsWith("РАЗДЕЛ") || text.startsWith("Раздел")) {
+                currentSection = { title: text, chapters: [] };
+                result.sections.push(currentSection);
+                currentChapter = null;
+                currentArticle = null;
+            } else if (text.startsWith("ГЛАВА") || text.startsWith("Глава")) {
+                currentChapter = { title: text, articles: [] };
+                if (currentSection) {
+                    currentSection.chapters.push(currentChapter);
+                } else {
+                    currentSection = { title: "", chapters: [currentChapter] };
+                    result.sections.push(currentSection);
+                }
+                currentArticle = null;
+            } else if (text.startsWith("Статья")) {
+                currentArticle = { title: text, clauses: [] };
+                if (currentChapter) {
+                    currentChapter.articles.push(currentArticle);
+                } else {
+                    currentChapter = { title: "", articles: [currentArticle] };
+                    if (currentSection) {
+                        currentSection.chapters.push(currentChapter);
+                    } else {
+                        currentSection = { title: "", chapters: [currentChapter] };
+                        result.sections.push(currentSection);
+                    }
+                }
+            }
+        } else if (currentArticle) {
+            currentArticle.clauses.push(text);
+        } else if (currentChapter) {
+            currentChapter.articles.push({ title: "", clauses: [text] });
+        } else if (currentSection) {
+            currentSection.chapters.push({ title: "", articles: [{ title: "", clauses: [text] }] });
+        } else {
+            result.preamble.push(text);
+        }
+    }
+
+    return result;
 }
 
-const html = fs.readFileSync('./data/html/constitution.html', 'utf8');
+// const documentId = 102027595;
+const documentId = 102041891;
+
+const html = fs.readFileSync(`./data/html/${documentId}.html`, 'utf8');
 
 const json = htmlToJson(html);
 // console.log('json', json);
 // console.log('json.sections[0].chapters', json.sections[0].chapters);
 
-saveFile('./data/json/constitution.json', JSON.stringify(json, null, 2));
+saveFile(`./data/json/${documentId}.json`, JSON.stringify(json, null, 2));
